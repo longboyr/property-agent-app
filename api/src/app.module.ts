077dded1e1a23dfcf3f6620581f448d46ef6e10b
longboyr <aesrespecia@gmail.com>
2025-06-26 20:36:11 +0800

    feat[api]: add other resource property, family, tenant and note

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PropertyAgentModule } from './property-agent/property-agent.module';
+import { PropertyModule } from './property/property.module';
+import { FamilyModule } from './family/family.module';
+import { TenantModule } from './tenant/tenant.module';
+import { NoteModule } from './note/note.module';
 
 @Module({
-  imports: [PropertyAgentModule],
+  imports: [PropertyAgentModule, PropertyModule, FamilyModule, TenantModule, NoteModule],
   controllers: [AppController],
   providers: [AppService],
 })
